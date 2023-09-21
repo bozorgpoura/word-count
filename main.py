1f70ca4c4f1c834034356b647628adffee55d11e
Amirhossein Bozorgpour <bozorgpoura@educ.college-montreal.qc.ca>
2023-09-21 11:17:09 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Nom: Amir Bozorgpour
+# 2023-09-13
+# TP1
+
+
+def count_word(input_string): # Définit une fonction 'count_word' qui prend une 'input_string' comme argument.
+    words = len(input_string.split())  # Divisez la chaîne en mots en utilisant l'espace comme séparateur
+    return words
+
+
+
